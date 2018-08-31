8ee90632125d3babe7fda891e0198827aa278214
sumi <GHOST@GHOSTdeMacBook-Pro.local>
2018-08-31 15:51:25 +0800

    -

--- src/common/goodsDeliver.vue
+++ src/common/goodsDeliver.vue
@@ -22,7 +22,7 @@
 			<div class="item-bottom">
 				<div class="convert">
 					兑换王国币
-					<span>兑换999币</span>
+					<span @click="exchange">兑换999币</span>
 				</div>
 				炫耀一下
 				<div class="float-right">></div>
@@ -41,15 +41,49 @@
 				上海市上海市浦东新区民生路1403号信息大厦2903室	
 			</div>
 		</div>
+		<div class="popout" v-show="convert">
+			<div class="box">
+				<div class="title">确定将物品兑换为999王国币吗?</div>
+				<div class="bottom">
+					<div class="puplic-btn cancel" @click="exchange">取消</div>
+					<div class="puplic-btn enter">确认</div>
+				</div>
+			</div>
+		</div>
+		<v-card class="card-style">
+			<img src="static/images/hall/pay/gold_3.png" class="gold-img" />
+			<div class="gold-style"><img src="static/images/hall/dailyBonus/coin.png" class="img-style" /><span>+ 7777</span></div>
+			兑换成功
+		</v-card>
+		<!-- <v-card class="card-style">
+			<div class="title">请选择您一同运送的物品</div>
+			<ul>
+				<li>娃娃1</li>
+				<li>娃娃2</li>
+				<li>娃娃3</li>
+				<li>娃娃4</li>
+			</ul>
+			<div class="btn">提交</div>
+		</v-card> -->
 	</div>
 </template>
 
 <script>
+	import Card from '@/components/card'
 	export default{
 		name : 'goodsDeliver',
 		data() {
 			return {
-				day : '14'
+				day : '14',
+				convert : false
+			}
+		},
+		components: {
+			'v-card' : Card
+		},
+		methods : {
+			exchange() {
+				this.convert = !this.convert
 			}
 		}
 	}
@@ -80,9 +114,9 @@
 	.goodsDeliver .head .title{
 		flex: auto;
 		margin: auto;
+		color: #999;
 		font-size: 14px;
 		font-weight: 400;
-		color: #999;
 		line-height: 24px;
 	}
 	.goodsDeliver .head .img-style{
@@ -137,7 +171,7 @@
 		font-weight: 400;
 		padding: 0 10px;
 		border-radius: 5px;
-		background-color: rgb(166,119,208)
+		background-color: rgb(166,119,208);
 	}
 	.goodsDeliver .float-right{
 		float: right;
@@ -177,5 +211,91 @@
 	}
 	.goodsDeliver .address .name{
 		margin-right: 40px;
+	}
+	.goodsDeliver .popout{
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		height: 100%;
+		background-color: rgba(0,0,0,.3);
+	}
+	.goodsDeliver .popout .box{
+		margin: 50vw 10vw;
+		padding: 10vw 4vw;
+		border-radius: 14px;
+		background-color: #fff;
+		color: #666;
+		font-size: 16px;
+	}
+	.goodsDeliver .popout .box .title{
+		margin-bottom: 30px;
+	}
+	.goodsDeliver .popout .bottom{
+		justify-content: space-between;
+		display: flex;
+		flex: 1;
+		padding: 0 10px;
+	}
+	.goodsDeliver .popout .puplic-btn{
+		width: 24vw;
+		line-height: 30px;
+		border-radius: 5px;
+		background-color: red;
+		color: #fff;
+		font-weight: bold;
+	}
+	.goodsDeliver .popout .cancel{
+		background-color: #ccc;
+	}
+	.goodsDeliver .popout .enter{
+		background-color: rgb(243,141,92);
+	}
+	.card-style{
+		font-weight: bold;
+	}
+	.card-style .gold-img{
+		position: absolute;
+		top: -24px;
+		left: 50%;
+		width: 100px;
+		margin-left: -50px;
+	}
+	.card-style .img-style{
+		width: 20px;
+		height: 20px;
+	}
+	.card-style .gold-style{
+		padding-top: 40px;
+		margin-bottom: 10px;
+		line-height: 50px;
+	}
+	.card-style span{
+		color: rgb(241,103,93);
+		font-weight: bold;
+		font-size: 24px;
+		margin-left: 10px;
+	}
+	.card-style .title{
+		text-align: left;
+		padding: 0 40px;
+		margin-bottom: 10px;
+		line-height: 50px;
+		border-bottom: 1px solid #eee;
+	}
+	.card-style li{
+		padding: 0 40px;
+		text-align: left;
+		color: rgb(118,119,120);
+		line-height: 28px;
+		font-weight: normal;
+	}
+	.card-style .btn{
+		background-color: rgb(252,133,78);
+		width: 30vw;
+		color: #fff;
+		line-height: 30px;
+		border-radius: 5px;
+		margin: 10px auto;
 	}
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="puplic-card">
+		<div class="inner">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name : 'card',
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.puplic-card{
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		background-color: rgba(0,0,0,.3);
+		height: 100%;
+	}
+	.puplic-card .inner{
+		margin: 30vw 10vw;
+		padding-bottom: 5vw;
+		border-radius: 14px;
+		font-size: 16px;
+		background-color: #fff;
+		color: #666;
+		position: relative;
+	}
+	
+</style>
