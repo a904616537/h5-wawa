e3c255ca5354653ed2f8e8df2df415327e63132d
sumi <GHOST@GHOSTdeMacBook-Pro.local>
2018-09-27 14:30:06 +0800

    week&month

--- src/common/recharge.vue
+++ src/common/recharge.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="recharge">
 		<v-nav>充值王国币</v-nav>
-		<div
+		<!-- <div
 		v-if="wawaplayer.recharge == 0"
 		class="banner"
 		style="background-image: url('./static/images/activity/firstpay/wawwjpayad.png')"
@@ -9,18 +9,36 @@
 		<div class="balance" v-else>
 			<div class="price">{{room_card}}</div>
 			<div class="massage">账户余额(王国币)</div>
+		</div> -->
+		<div class="balance">
+			<div class="price">{{room_card}}</div>
+			<div class="massage">账户余额(王国币)</div>
 		</div>
 		
 		<div class="gold-box">
 			<!-- 月卡 和 周卡 -->
-			<div class="item">
+			<div class="item-card">
 				<div class="inner" style="background-image: url('./static/images/top-up/week.png')">
-					<div class="glod">
-						<div class="float-left">周卡</div>
-						<div class="float-right">充380币</div>
+					<div class="gold">
+						<div class="float-item">
+							<div class="float-left" style="color: #FA7737">周卡</div>
+							<div class="float-right">充380币</div>
+						</div>
 						<div class="tip">送120币+包邮卡*1+vip瞄准点</div>
 					</div>
 					<div class="money">¥ 38</div>
+				</div>
+			</div>
+			<div class="item-card">
+				<div class="inner" style="background-image: url('./static/images/top-up/month.png')">
+					<div class="gold">
+						<div class="float-item">
+							<div class="float-left" style="color: #CF4BD5">月卡</div>
+							<div class="float-right">充380币</div>
+						</div>
+						<div class="tip">送960币+包邮卡*1+vip瞄准点</div>
+					</div>
+					<div class="money">¥ 120</div>
 				</div>
 			</div>
 
@@ -41,6 +59,15 @@
 				</div>
 			</div>
 		</div>
+
+		<!-- card 弹框 -->
+		<v-card v-if="show" class="card-style" :onPress="onClose">
+			<img :src="gold_bg" class="gold-img" />
+			<div class="gold-style"><img :src="coin" class="img-style" /><span>+{{golds}}</span></div>
+			<div class="tips">亲，今日周卡福利已送达</div>
+			<div class="tip-text">你还可以领取6天哦~</div>
+			<div class="btn">续卡</div>
+		</v-card>
 	</div>
 </template>
 
@@ -48,11 +75,16 @@
 	import Vue from 'vue';
 	import axios from 'axios';
 	import {mapState, mapGetters, mapActions} from 'vuex'
-	import payment from '@/utils/payment';
+	import payment from '@/utils/payment'
+	import Card from '@/components/card'
 	export default{
 		name : 'recharge',
 		data() {
 			return {
+				show    : true,
+				golds   : '270币',
+				gold_bg : './static/images/hall/pay/gold_3.png',
+				coin    : './static/images/hall/dailyBonus/coin.png',
 				bglist : new Map([
 					[0, "background-image: url('./static/images/hall/pay/gold_1.png')"],
 					[1, "background-image: url('./static/images/hall/pay/gold_2.png')"],
@@ -62,6 +94,9 @@
 					[5, "background-image: url('./static/images/hall/pay/gold_6.png')"],
 				])
 			}
+		},
+		components: {
+			'v-card' : Card
 		},
 		computed : {
 			...mapState({
@@ -107,6 +142,9 @@
 					this.isShowComfilm = true
 				})
 			},
+			onClose() {
+				this.show = false;
+			},
 		},
 		mounted() {
 			if(!this.pomelo_login) {
@@ -118,13 +156,13 @@
 
 <style>
 	.recharge{
+		background-color : #f2d56e;
+		position         : absolute;
 		top              : 0;
 		left             : 0;
 		right            : 0;
 		height           : 100%;
 		padding          : 0 8px;
-		position         : absolute;
-		background-color : #f2d56e;
 	}
 	.recharge .banner{
 		width               : 100%;
@@ -139,10 +177,15 @@
 	.recharge .balance {
 		color : #8e562a;
 	}
+	.recharge .balance .price{
+		font-size: 42px;
+		font-weight: bold;
+	}
 	.recharge .gold-box{
 		border-radius    : 20px;
-		padding-bottom   : 15vh;
+		padding-bottom   : 11vh;
 		background-color : #fff;
+		margin           : 3vw 0;
 	}
 	.recharge .gold-box:after{
 		height   : 0;
@@ -158,10 +201,53 @@
 		font-size : 14px;
 		color     : #8e562a;
 	}
+	.recharge .item-card{
+		color: #fff;
+		float: left;
+		width: 50%;
+		padding: 10px 0;
+		font-size: 12px;
+	}
+	.recharge .item-card .gold{
+		height           : 10vh;
+		padding          : 0 1vw;
+	}
 	.recharge .item .gold{
 		height           : 10vh;
 		padding          : 0 1vw;
 		background-color : #fef6e1;
+	}
+	.recharge .item-card .inner{
+		margin        : 0 10px;
+		border-radius : 10px;
+		overflow      : hidden;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	.recharge .item-card .float-item{
+		content: '';
+		display: block;
+		clear: both;
+		overflow: hidden;
+	}
+	.recharge .item-card .float-left{
+		float: left;
+		font-weight: bold;
+		font-size: 14px;
+		line-height: 30px;
+	}
+	.recharge .item-card .float-right{
+		float: right;
+		font-weight: bold;
+		font-size: 16px;
+		line-height: 30px;
+	}
+	.recharge .item-card .tip{
+		font-size: 10px;
+		line-height: 50px;
+	}
+	.recharge .item-card .money{
+		background-color: transparent;
 	}
 	.recharge .item .inner{
 		margin        : 0 10px;
@@ -195,8 +281,67 @@
 	.recharge .money{
 		color            : #fff;
 		font-size        : 16px;
-		line-height      : 30px;
+		line-height      : 24px;
 		font-weight      : bold;
 		background-color : #da551f;
 	}
+	/* card 弹框 */
+	.card-style{
+		font-weight: bold;
+	}
+	.card-style .gold-img{
+		position: absolute;
+		top: -24px;
+		left: 50%;
+		width: 100px;
+		margin-left: -50px;
+	}
+	.card-style .img-style{
+		width: 20px;
+		height: 20px;
+	}
+	.card-style .gold-style{
+		padding-top: 40px;
+		line-height: 50px;
+	}
+	.card-style span{
+		color: rgb(241,103,93);
+		font-weight: bold;
+		font-size: 32px;
+		margin-left: 10px;
+	}
+	.card-style .title{
+		text-align: left;
+		padding: 0 40px;
+		margin-bottom: 10px;
+		line-height: 50px;
+		border-bottom: 1px solid #eee;
+	}
+	.card-style .tip-text{
+		color: #F1675C;
+		font-size: 14px;
+		margin-top: 10px;
+	}
+	.card-style li{
+		padding     : 0 40px;
+		text-align  : left;
+		color       : #666;
+		line-height : 28px;
+		font-weight : normal;
+	}
+	.card-style li.active{
+		color: #f2d56e;
+	}
+	.card-style li i{ 
+		float : right;
+	}
+	.card-style .btn{
+		background-color: rgb(252,133,78);
+		width: 30vw;
+		color: #fff;
+		line-height: 30px;
+		border-radius: 5px;
+		margin: 10px auto;
+	}
+	-webkit-text-size-adjust:none;
 </style>
